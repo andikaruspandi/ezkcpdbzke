111d3377a8ecdeb73f700d720167cd6325bf3dda
Andika Ruspandi <andika.ruspandi1993@gmail.com>
2021-09-23 00:10:19 +0700

    Update validator in middleware

--- app/routes.js
+++ app/routes.js
@@ -16,8 +16,18 @@ $routes.init = () => {
   app.get('/products', fn.loggedIn, product.getData);
   app.get('/products/total', fn.loggedIn, product.getTotalDataByPrice);
   app.get('/products/:id', fn.loggedIn, product.getDataById);
-  app.post('/products', fn.loggedIn, product.insert);
-  app.post('/products/:id', fn.loggedIn, product.update);
+  app.post('/products', 
+    fn.loggedIn, 
+    body('name').isLength({ min:3 }),
+    body('price').isLength({ min:1 }),
+    product.insert
+  );
+  app.post('/products/:id', 
+    fn.loggedIn, 
+    body('name').isLength({ min:3 }),
+    body('price').isLength({ min:1 }),
+    product.update
+  );
   app.post('/products/delete/:id', fn.loggedIn, product.delete);
 
   //auth
@@ -25,9 +35,19 @@ $routes.init = () => {
   app.post('/login', fn.checkLogged, auth.login);
   app.post('/register', 
     fn.checkLogged,
+    body('name').isLength({ min:3 }),
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
-    auth.register);
+    body('password').custom((value, { req }) => {
+      if (value !== req.body.repassword) {
+        throw new Error('Password confirmation does not match password');
+      }
+
+      // Indicates the success of this synchronous custom validator
+      return true;
+    }),
+    auth.register
+  );
   app.get('/delete-account', fn.checkLogged, auth.deleteAccountView);
   app.post('/delete-account', fn.checkLogged, auth.deleteAccount);
   app.get('/logout', fn.loggedIn, auth.logout);
